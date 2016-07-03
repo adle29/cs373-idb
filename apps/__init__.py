52a1bf296ede86999b061bffc32dd56e011c08b4
Dimitri Vasiliev <sniper@citadel.attlocal.net>
2016-07-03 01:05:10 -0500

    added routes interfaces, included bootstrap, added angular infraestructure

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,28 +1,49 @@
 #!flask/bin/python
 from flask import Flask
-from flask import render_template
+from flask import render_template, send_file
+import requests
+import ast
 
 app = Flask(__name__)
 
+headers = { 'X-Auth-Token': '1a83f9cdfa664421bc7c1997f1409218', 'X-Response-Control': 'minified' }
+
 @app.route('/')
-def index(name=None):
-    return render_template('index.html', name=name)
-
-@app.route('/players')
-def players(name=None):
-    return render_template('players.html', name=name)
-
-@app.route('/teams')
-def teams(name=None):
-    return render_template('teams.html', name=name)
+def index():
+    return send_file("templates/index.html")
 
 @app.route('/seasons')
-def seasons(name=None):
-    return render_template('seasons.html', name=name)
+def seasons():
+    r = requests.get('http://api.football-data.org/v1/competitions/?season=2016', headers=headers)
+    return r.text
 
-@app.route('/about')
-def about(name=None):
-    return render_template('about.html', name=name)
+@app.route('/season/<season_id>')
+def season(season_id):
+    r = requests.get('http://api.football-data.org/v1/competitions/'+season_id+'/leagueTable', headers=headers)
+    return r.text
+
+@app.route('/team/<team_id>')
+def team(team_id):
+    r = requests.get('http://api.football-data.org/v1/teams/'+team_id, headers=headers)
+    return r.text
+
+@app.route('/players/<team_id>')
+def players(team_id):
+    r = requests.get('http://api.football-data.org/v1/teams/'+team_id+'/players', headers=headers)
+    return r.text
+
+@app.route('/games')
+def games():
+    r = requests.get('http://api.football-data.org/v1/fixtures/', headers=headers)
+    return r.text
+#
+# @app.route('/seasons')
+# def seasons(name=None):
+#     return render_template('seasons.html', name=name)
+#
+# @app.route('/about')
+# def about(name=None):
+#     return render_template('about.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ apps/static/js/app.js
@@ -0,0 +1,192 @@
+'use strict';
+
+var myApp = angular.module('golazoApp',['ngRoute'])
+
+var routes = myApp.config(['$httpProvider', function($httpProvider) {
+  $httpProvider.defaults.useXDomain = true;
+  $httpProvider.defaults.withCredentials = true;
+  delete $httpProvider.defaults.headers.common["X-Requested-With"];
+  $httpProvider.defaults.headers.common["Accept"] = "application/json";
+  $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
+}])
+
+.config(["$routeProvider",
+  function($routeProvider){
+    $routeProvider.
+    when('/',{
+      templateUrl: '/static/partials/index.html',
+      controller: 'main'
+    }).
+    when('/about',{
+      templateUrl: '/static/partials/about.html'
+    })
+    .when('/games',{
+      templateUrl: '/static/partials/games.html',
+      controller: 'games'
+    })
+    .when('/seasons',{
+      templateUrl: '/static/partials/seasons.html',
+      controller: 'seasons'
+    })
+    .when('/season/:id',{
+      templateUrl: '/static/partials/season.html',
+      controller: 'season'
+    })
+    .when('/team/:id',{
+      templateUrl: '/static/partials/team.html',
+      controller: 'team'
+    })
+    .otherwise({redirectTo:'/'})
+  }]);
+
+routes.controller('main',['$scope', '$http',function($scope, $http){
+  $scope.games = [];
+  $scope.show_card = false;
+
+  $http.get('/games').then(function(response){
+    console.log(response);
+    var res = response.data.fixtures;
+    for (var i = 0; i < res.length; i++) {
+        var element = res[i];
+        var game = {
+          date: element.date,
+          awayTeamName: element.awayTeamName,
+          homeTeamName: element.homeTeamName
+        };
+        $scope.games.push(game);
+    }
+  });
+
+  if ($scope.games.length <2)
+    $scope.show_card = true;
+}]);
+
+routes.controller('team',['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
+  var id = $routeParams.id;
+
+  var fetchPlayers = function(team_id){
+    $http.get("/players/"+team_id).then(function(response){
+     console.log(response);
+   });
+ };
+
+  $http.get('/team/'+id).then(function(response){
+    var res = response.data;
+    console.log(res);
+    $scope.team = {
+      name:res.name,
+      logo:res.crestUrl
+    };
+
+    fetchPlayers(id);
+
+  });
+
+}]);
+
+routes.controller('season',['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
+  var id = $routeParams.id;
+
+  $http.get('/season/'+id).then(function(response){
+
+    var res = response.data;
+
+    console.log(res);
+    var standings = res.standing;
+
+    $scope.leagueCaption = res.leagueCaption;
+    $scope.groups = [];
+
+
+
+    if( typeof standings == "undefined" ) {
+      var group = {
+        letter: groupLetter,
+        teams: []
+      };
+
+      for (var i in standings){
+        var team = standings[i];
+        console.log( i );
+        var newTeam = {
+          logo:team["crestURI"],
+          name:team["team"],
+          id:team["teamId"],
+          rank:team["rank"]
+        };
+        group.teams.push(newTeam);
+      }
+
+      $scope.groups.push(group);
+    }
+    else {
+      for (var groupLetter in standings) {
+
+        var rawGroupData = standings[groupLetter];
+        var group = {
+          letter: groupLetter,
+          teams: []
+        };
+
+        for (var i in rawGroupData) {
+          var team = rawGroupData[i];
+
+          var newTeam = {
+            logo:team.crestURI,
+            name:team.team,
+            id:team.teamId,
+            rank:team.rank
+          };
+
+          group.teams.push(newTeam);
+        }
+
+        $scope.groups.push(group);
+      }
+    }
+
+  });
+
+}]);
+
+
+routes.controller('seasons',['$scope', '$http', function($scope, $http){
+
+  $scope.seasons = [];
+
+
+  $http.get('/seasons').then(function(response){
+    console.log(response);
+    var res = response.data;
+    for (var i = 0; i < res.length; i++) {
+        var element = res[i];
+        var season = {
+          name: element.caption,
+          league: element.league,
+          id: element.id,
+          year: element.year
+        };
+        $scope.seasons.push(season);
+    }
+  });
+
+}]);
+
+routes.controller('games',['$scope', '$http', function($scope, $http){
+  $scope.games = [];
+
+  $http.get('/games').then(function(response){
+    console.log(response);
+    var res = response.data.fixtures;
+    for (var i = 0; i < res.length; i++) {
+        var element = res[i];
+        var game = {
+          date: element.date,
+          awayTeamName: element.awayTeamName,
+          homeTeamName: element.homeTeamName
+        };
+        $scope.games.push(game);
+    }
+    console.log($scope.games);
+  });
+}]);

4a1d24579feb700e60d2358e1ee36744a9544952
Dimitri Vasiliev <sniper@wireless-10-147-49-72.public.utexas.edu>
2016-07-05 17:32:46 -0500

    improved website design, added static content

--- apps/models.py
+++ apps/models.py
@@ -1,34 +1,41 @@
- import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
 
-# Seasons
-    # Season_id #connected to games
-    # List of teams #connected to teams
-    # Name of the Season
-    # Name of the league
-    # Year
-    # Standings
-    #
-
-# Games
-    # Season_id #connected to seasons
-    # Date
-    # result
-    # homeTeamId #connected to teams
-    # awayTeamId  #connected to teams
-    # Result
-
-
-# Teams
-    # Team_id
-    # Name
-    # Logo_url
-    # List of player ids #connected to player
-    # List of games ids #contect to Games
-
-
-# Player
-    # Player_id
-    # Team_id  #connected to team
-    #  name
-    # Position
-    # Nationality
+# Base = declarative_base()
+#
+# class Season(Base):
+#     __tablename__ = "seasons"
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String)
+#
+# class Standing(Base):
+#     id = Column(Integer, primary_key=True)
+#     team_name =
+#     team_id =
+#     group_letter =
+#     seasons_id = Column(Integer, primary_key=True)
+#     playedGames: team.playedGames,
+#     goalsAgainst: team.goalsAgainst,
+#     rank =
+#     goals: team.goals,
+#     pts: team.points
+#
+# class Teams(Base):
+#     __tablename__ = "seasons"
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String)
+#     logo = Column(String)
+#
+# class Games(Base):
+#     __tablename__ = "seasons"
+#     id = Column(Integer, primary_key=True)
+#     date =
+#     awayTeamName =
+#     homeTeamName =
+#
+# class Player(Base):
+#     __tablename__ = "seasons"
+#     id = Column(Integer, primary_key=True)
+#     team_id = Column(Integer, primary_key=True)
+#     name = Column(String)
+#     position = Column(String)
+#     nationality = Column(String)

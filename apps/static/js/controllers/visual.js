84804d43a0e0e8c286e1d78a54b167b06b25ee66
Abraham Adberstein <aadberstein@gmail.com>
2016-07-21 18:06:11 -0500

    master piece of d3

--- apps/static/js/controllers/visual.js
+++ apps/static/js/controllers/visual.js
@@ -1,16 +1,15 @@
 routes.controller('visual',['$scope', '$http', '$rootScope', function($scope, $http, $rootScope){
 
-
-	// houses
-	// characters
-	//events
-
-	//make characters link to houses, houses link to events 
 	var urlCharacters = "/static/js/data/characters.json";
 	var urlHouses = "/static/js/data/houses.json";
 
 	$scope.characters = []; 
 	$scope.houses = []; 
+	$scope.data = {};
+
+	$scope.mixedData = {};
+	$scope.idCharacters = {};
+	$scope.clickId = "here";
 
 	var model = {};
 
@@ -22,84 +21,67 @@ routes.controller('visual',['$scope', '$http', '$rootScope', function($scope, $h
 	    return Math.random() * (max - min) + min;
 	}
 
+	function isInArray(value, array) {
+	  return array.indexOf(value) > -1;
+	}
+
+
 	$http.get(urlHouses).then(function(res1){
 		$http.get(urlCharacters).then(function(res2){
           var houses = res1.data;
           var characters =  res2.data; 
-
+          $scope.characters = characters; 
+		  $scope.houses = houses; 
           console.log(houses[0]); 
           console.log(characters[0]); 
 
           var colors = {};
+          var doNotIncludeCharacters = [];
+
+           for(var i = 0; i < characters.length; i++){
+      	  	var character = characters[i];
+      	  	if (character.name == ""){
+      	  		doNotIncludeCharacters.push( parseInt(character.id) );
+      	  	}
+      	  }
 
           for(var i = 0; i < houses.length; i++){
           	var house = houses[i];
-          	var newNode = { "id":house.name, "group": i};
-    
+          	var newNode = { "id":house.name, "group": i, "name": house.name.slice(6), "type":"house"};
+    		
           	jsonData["nodes"].push(newNode)
 
           	var swornMembers = house.swornMembers; 
 
+          	$scope.mixedData[house.name.slice(6)] = swornMembers;
+
           	for(var j = 0; j < swornMembers.length; j++){
           		colors[swornMembers[j]] = i;
-  				var newLink = {"source":house.name, "target": swornMembers[j], "value": getRandomArbitrary(1, 9), "name":house.name, "type":"house"};
-  				jsonData["links"].push(newLink);
+          		if ( isInArray(parseInt(swornMembers[j]), doNotIncludeCharacters) == false ) {
+  					var newLink = {"source":house.name, "target": swornMembers[j], "value": getRandomArbitrary(1, 9), "name":house.name.slice(6)};
+  					jsonData["links"].push(newLink);
+  				}
   			} //small for
 
       	  } //end big for
 
       	  for(var i = 0; i < characters.length; i++){
       	  	var character = characters[i];
-          	var newNode = { "id":character.id, "group": colors[character.id], "name":character.name, "type":"character"};
-          	jsonData["nodes"].push(newNode);
+      	  	if ( isInArray( parseInt(character.id), doNotIncludeCharacters) == false ) {
+      	  		$scope.idCharacters[character.id] = character.name;
+          		var newNode = { "id":character.id, "group": colors[character.id], "name":character.name, "type":"character"};
+          		jsonData["nodes"].push(newNode);
+          	}
       	  }
 
-      	  console.log(jsonData);
 
-      	  ForceDirectedVisual(jsonData);
+      	  ForceDirectedVisual(jsonData, $scope.mixedData, $scope.idCharacters);
+      	  $scope.data = JSON.stringify(jsonData, null, 2);
         });               
     });
 
 
-	// var jsonData = {
-	// 	  "nodes": [
-	// 	    {"id": "Myriel", "group": 1},
-	// 	    {"id": "Napoleon", "group": 1},
-	// 	    {"id": "Mlle.Baptistine", "group": 1},
-	// 	    {"id": "Mme.Magloire", "group": 1},
-	// 	    {"id": "CountessdeLo", "group": 1},
-	// 	    {"id": "Geborand", "group": 1},
-	// 	    {"id": "Champtercier", "group": 1},
-	// 	    {"id": "Cravatte", "group": 1},
-	// 	    {"id": "Count", "group": 1},
-	// 	    {"id": "OldMan", "group": 1},
-	// 	    {"id": "Labarre", "group": 2},
-	// 	    {"id": "Valjean", "group": 2},
-	// 	    {"id": "Marguerite", "group": 3}
 
-	// 	    ],
-	// 	  "links": [
-	// 	  	{"source": "Napoleon", "target": "Myriel", "value": 1}
- //    	]};
-
-  //   var myFlower = new ForceDirectedVisual();
-		// myFlower.update(jsonData);
-		
-	// { 
-	// 	node: {
-	// 		name: house of this
-	// 		link: character1
-	// 		link: character2
-	// 		link: fight
-	// 	}
-
-	// 	node: {
-	// 		name: fight of something
-	// 		link: house of this
-	// 		link: house of that
-	// 	}
-	// }
-  
 
 
 
--- apps/static/js/d3/CodeFlower.js
+++ apps/static/js/d3/CodeFlower.js
@@ -1,39 +1,103 @@
-var ForceDirectedVisual = function(graph){
+var ForceDirectedVisual = function(graph, mixedData, idCharacters){
   
   var svg = d3.select("svg"),
       width = $("svg").parent().width(),//svg.attr("width"),
       height = $("svg").parent().height();//svg.attr("height");
 
   var color = d3.scaleOrdinal(d3.schemeCategory20);
-
+  var BOUNDS = 0.4
   var simulation = d3.forceSimulation()
       .force("link", d3.forceLink().id(function(d) { return d.id; }))
       .force("charge", d3.forceManyBody())
-      .force("x", d3.forceX().strength(0.002))
-    .force("y", d3.forceY().strength(0.002))
-      .force("center", d3.forceCenter(width / 2, height / 2));
+      .force("x", d3.forceX().strength(BOUNDS))
+      .force("y", d3.forceY().strength(BOUNDS))
+      .force("center", d3.forceCenter(width / 3, height / 2));
 
   var link = svg.append("g")
       .attr("class", "links")
     .selectAll("line")
     .data(graph.links)
     .enter().append("line")
-      .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+      .attr("stroke-width", function(d) { return 1; });
 
+
+      
   var node = svg.append("g")
       .attr("class", "nodes")
     .selectAll("circle")
     .data(graph.nodes)
     .enter().append("circle")
-      .attr("r", 5)
+      .attr("r", 3)
       .attr("fill", function(d) { return color(d.group); })
+
+      .on("click", function(d){
+         if( d.type == "house"){
+             var name = d.name;
+             console.log(color(d.group));
+             var text = "<div class='insideDiv'><h3 style=' color:"+ color(d.group) +";'>" +d.name + "</h3><ul>";
+
+             var members = mixedData[d.name];
+
+             console.log(d.name, members);
+
+             for(var i = 0; i < members.length; i++){
+                var memberID = members[i];
+                text += "<li><small>"+idCharacters[memberID] + "</small></li>";
+             }  
+
+             var legend = svg.select("#legend");
+
+             text += "<lu></div>"
+
+             legend.html(text);
+
+             // var div = legend.append('xhtml:div')
+             //            .append('div').append('p').html(text);
+
+         }
+
+      })
       .call(d3.drag()
           .on("start", dragstarted)
           .on("drag", dragged)
           .on("end", dragended));
 
-  node.append("title")
-      .text(function(d) { return d.id; });
+  node.append("text")
+    .text(function(d) { return d.name; });
+
+
+  var texts = svg.selectAll("text.label")
+            .data(graph.nodes)
+            .enter().append("text")
+            .attr("class", "label")
+            .attr("fill", function(d){
+              if (d.type == "house")
+                return color(d.group);
+              else
+                return "black";
+            })
+            .attr("font-size", "8px")
+            .attr("visibility", function(d){
+              if (d.type == "house")
+                return "visible";
+              else
+                return "hidden";
+            })
+            .text(function(d) {  return d.name;  });
+
+
+    svg.append("foreignObject")
+        .attr("id", "legend")
+        .attr("width", "300px")
+        .attr("height", "700px")
+        .style("border", "solid black 1px")
+        .attr("x", ($("svg").parent().width() - 450) + "px")
+        .attr("y", "30px").html("Click a house to display its members.");
+
+   var control = svg.append("foreignObject")
+      .attr("id", "control")
+      .attr("x", "30px")
+      .attr("y", "30px");
 
   simulation
       .nodes(graph.nodes)
@@ -41,6 +105,42 @@ var ForceDirectedVisual = function(graph){
 
   simulation.force("link")
       .links(graph.links);
+
+
+  var html = '<input name="updateButton" class="btn" type="button" value="Zoom +" onclick="zoomPlus()" /><input name="updateButton" class="btn" type="button"  value="Zoom -" onclick="zoomMinus()" />';
+
+  control.append('xhtml:div').append("input")
+    .attr("class", "btn")
+    .attr("value", "ZOOM -")
+    .style("float", "left")
+    .style("margin", "10px")
+    .attr("type","button")
+    .on("click", function(){
+      zoomPlus();
+    });
+
+  control.append('xhtml:div').append("input")
+    .attr("class", "btn")
+    .attr("value", "ZOOM +")
+    .style("margin", "10px")
+    .attr("type","button")
+    .on("click", function(){
+      zoomMinus();
+    });
+
+
+  function zoomPlus(){
+    BOUNDS += 0.1;
+    simulation.force("x", d3.forceX().strength(BOUNDS))
+              .force("y", d3.forceY().strength(BOUNDS));
+    console.log("click");
+  }
+  
+  function zoomMinus(){
+    BOUNDS -= 0.1;
+    simulation.force("x", d3.forceX().strength(BOUNDS))
+              .force("y", d3.forceY().strength(BOUNDS));
+  }
 
   function ticked() {
     link
@@ -52,6 +152,10 @@ var ForceDirectedVisual = function(graph){
     node
         .attr("cx", function(d) { return d.x; })
         .attr("cy", function(d) { return d.y; });
+
+    texts.attr("transform", function(d) {
+        return "translate(" + d.x + "," + d.y + ")";
+    });
   }
 
   function dragstarted(d) {

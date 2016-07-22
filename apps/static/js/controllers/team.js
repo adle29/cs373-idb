ae143506d42879b3e05c2b15cb9689aa35a55f4b
Abraham Adberstein <aadberstein@gmail.com>
2016-07-21 20:50:19 -0500

    fixed link errors

--- apps/static/js/controllers/team.js
+++ apps/static/js/controllers/team.js
@@ -38,6 +38,8 @@ routes.controller('team',['$scope', '$http', '$routeParams', function($scope, $h
           home_team_name: game.home_team_name,
           home_team_score:game.home_team_score,
           away_team_score: game.away_team_score,
+          home_team_id:game.home_team_id,
+          away_team_id: game.away_team_id,
           date: game.date,
           id: game.id,
           season: game.season
